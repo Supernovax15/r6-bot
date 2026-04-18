fbbbe00d230a04a26e7f8b033bde321db11d22c0
Supernovax15 <nic.felice123@gmail.com>
2026-04-18 22:32:59 +0200

    add port fix

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Bot runs');
+}).listen(PORT, () => {
+  console.log(`Webserver runs on Port ${PORT}`);
+});
+
 require('dotenv').config();
 
 const { Client, GatewayIntentBits } = require('discord.js');
