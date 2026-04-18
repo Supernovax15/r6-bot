9488d4a7a3beac5987e450b1c9657a5b66c841a8
Supernovax15 <nic.felice123@gmail.com>
2026-04-18 23:22:23 +0200

    fix voice detection

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]
 });
 
 //load Ops
