337a246c00567ca010c6d1da49ee9667a3bd7e56
Supernovax15 <nic.felice123@gmail.com>
2026-04-18 21:46:46 +0200

    first commit

---
+++ deploy-command.js
@@ -0,0 +1,44 @@
+
+require('dotenv').config();
+
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('spin')
+    .setDescription('Bekomme einen zufälligen Operator')
+    .addStringOption(option =>
+      option.setName('side')
+            .setDescription('Wähle Attacker oder Defender')
+            .addChoices(
+            { name: 'Attacker', value: 'att' },
+            { name: 'Defender', value: 'def' }
+            )
+            .setRequired(true)),
+
+    new SlashCommandBuilder()
+            .setName('teamspin')
+            .setDescription('Gibt allen im Voice Channel einen zufälligen Operator'),
+
+    new SlashCommandBuilder()
+            .setName('reroll')
+            .setDescription('Erlaubt dir einen neuen Operator (einmalig!)')
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log('Registriere Commands');
+
+        await rest.put(
+            Routes.applicationCommands('1495123609985880345'),
+        { body: commands }
+        );
+
+        console.log('Commands registriert');
+    }   catch (error) {
+    console.error(error);
+    }
+})();
+
---
+++ index.js
@@ -0,0 +1,101 @@
+require('dotenv').config();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+//load Ops
+const operators = require('./data/operators');
+
+//Safe reroll
+const lastPicks = new Map();
+const rerollUsed = new Map();
+
+
+client.once('clientReady', () => {
+  console.log('Bot ist online!');
+});
+
+//Commands
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  // /spin
+  if (interaction.commandName === 'spin') {
+    const side = interaction.options.getString('side');
+
+    let pool = side === 'def'
+      ? operators.defenders
+      : operators.attackers;
+
+    const pick = pool[Math.floor(Math.random() * pool.length)];
+
+    lastPicks.set(interaction.user.id, { side });
+    rerollUsed.set(interaction.user.id, false);
+
+    await interaction.reply(`🎯 Dein Operator: **${pick}**`);
+  }
+
+  // /reroll
+  if (interaction.commandName == 'reroll') {
+    const userId = interaction.user.id;
+
+  if (!lastPicks.has(userId)) {
+    return interaction.reply('Zuerst /spin benutzen bittöö');
+  }
+
+  if (rerollUsed.get(userId)) {
+    return interaction.reply('Reroll schon verwendet... damn');
+  }
+
+  const { side } = lastPicks.get(userId);
+
+  let pool = side === 'def'
+    ? operators.defenders
+    : operators.attackers;
+
+  const pick = pool[Math.floor(Math.random() * pool.length)];
+
+  rerollUsed.set(userId, true);
+
+  await interaction.reply(`🔁 Neuer Operator: **${pick}**`);
+
+}
+
+  // /teamspin
+  if (interaction.commandName === 'teamspin') {
+    const voiceChannel = interaction.member.voice.channel;
+
+    if (!voiceChannel) {
+        return interaction.reply('Bitte trete zuerst einem Voice bei"');
+    }
+
+    const members = voiceChannel.members;
+
+    if (member.size === 0) {
+        return interaction.reply('Niemand im Voicechannel');
+    }
+
+    let result = '🎰 **Team Operator Picks:**\n\n'
+
+    members.forEach(member => {
+        if (member.user.bot) return;
+
+        const isDef = Math.random() < 0.5;
+
+        const pool = isDef 
+            ? operators.defenders
+            : operators.attackers;
+
+        const pick = pool[Math.floor(Math.random() * pool.length)];
+
+        result += `${member.user.username} → **${pick}** (${isDef ? 'DEF' : 'ATT'})\n`;
+    });
+
+    await interaction.reply(result);
+  }    
+});
+
+client.login(process.env.TOKEN);
